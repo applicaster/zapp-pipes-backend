c59098e4212d3a59496562ca5ce337cdb79689d7
zloof <nirzloof@gmail.com>
2020-04-10 22:48:49 +0300

    cleanup for firebase deployment

--- .circleci/deploy_to_firebase.sh
+++ .circleci/deploy_to_firebase.sh
@@ -14,10 +14,10 @@ APP_NAME="$1"
 [ "$FIREBASE_TOKEN" == "" ] && fail "required: <FIREBASE_TOKEN> params"
 
 echo "install nodejs zip unzip"
-# curl -sL https://deb.nodesource.com/setup_12.x | bash -
-# apt-get install -y nodejs unzip zip
-# npm install yaml@1.8.3
-# npm install -g firebase-tools@7.16.2
+curl -sL https://deb.nodesource.com/setup_12.x | bash -
+apt-get install -y nodejs unzip zip
+npm install yaml@1.8.3
+npm install -g firebase-tools@7.16.2
 
 FOLDER_PATH="apps/${APP_NAME}/"
 echo "deploy_to_firebase.js"
@@ -25,5 +25,5 @@ node .circleci/deploy_to_firebase.js $APP_NAME $FOLDER_PATH
 cd $FOLDER_PATH
 echo "firebase deploy"
 FIREBASE_PROJECT=applicaster-audience
-firebase deploy --project $FIREBASE_PROJECT --token $FIREBASE_TOKEN
+firebase deploy --project applicaster-audience --token $FIREBASE_TOKEN
 # firebase emulators:start --project applicaster-audience
